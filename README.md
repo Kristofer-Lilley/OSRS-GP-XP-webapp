3e7138be119b44545e64cf8ea673dbacb4746b60
Carthelan <klilley64@hotmail.com>
2025-08-26 12:19:44 -0600

    got sorting by item IDs working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 readme
+https://prices.runescape.wiki/api/v1/osrs/5m?timestamp=1615733400
+https://prices.runescape.wiki/api/v1/osrs/mapping
+https://oldschool.runescape.wiki/?title=Module:GEIDs/data.json&action=raw&ctype=application%2Fjson
--- osrs-gp-xp-webapp/src/App.js
+++ osrs-gp-xp-webapp/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import itemIdList from './ItemID.json';
 
 
 
@@ -9,7 +9,18 @@ function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [itemIds, setItemIds] = useState(itemIdList);
   const customUserAgent = 'OSRS-GP-XP-Webapp/0.1, Discord: Shirt9276, Training calculator for Old School RuneScape';
+
+  function handleItemIDChange() {
+    const sortedEntries = Object.entries(itemIds).sort((a, b) => a[1] - b[1]);
+    const swappedEntries = sortedEntries.map(([key, value]) => [value, key]);
+    const sortedJsonObject = Object.fromEntries(swappedEntries);
+    setItemIds(sortedJsonObject);
+    console.log(sortedJsonObject)
+  }
+
+
 
   useEffect(() => {
     const fetchData = () => {
@@ -23,21 +34,21 @@ function App() {
       .then((res) => setData(res.data))
       .catch((err) => setError(err))
       .finally(() => setLoading(false));
-    }}, []);
+    };
+    handleItemIDChange();
+    fetchData();
+    }, []);
 
     if (loading) {
       return <div>Loading...</div>;
     } else {
     return (
       <div className="App">
-        {
-          data.map((data) => (
-            <div key={data.id}>
-              <h2>{data.name}</h2>
-              <p>Price: {data.price}</p>
-            </div>
-          ))
-        }
+        {Object.entries(itemIds).map(([key, val]) => (
+          <p key={key}>
+          {val}
+        </p>
+      ))}
       </div>
       );
     }
